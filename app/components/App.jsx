c967a30245eba8bce297eadd2a160cb202c60d8e
Henri Huuskonen <henri.huuskonen@hotmail.com>
2020-10-28 15:39:17 +0200

    Add responsiveness and modifications

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -14,9 +14,10 @@ export default class App extends React.Component {
         <Hero title="HERO" name="HENRI HUUSKONEN" paragraph="Software developer, tech enthusiast & a man of too many projects"/>
         <div className="content__row">
           <Stats title="STATS"/>
-          <Bio title="BIO"/>
+          <Bio title="BIO" id="bio-row"/>
           <Socials title="SOCIALS"/>
         </div>
+        <Bio title="BIO" id="bio-block"/>
         <Projects title="PROJECTS"/>
         <Footer/>
       </div>
--- app/components/Bio.jsx
+++ app/components/Bio.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default class Bio extends React.Component {
   render() {
     return (
-      <div id="bio" className="title-container">
+      <div id={this.props.id} className="title-container">
         <p className="title-container__title">{this.props.title}</p>
         <div className="title-container__inner">
           <p>Software developer, tech enthusiast & a man of too many projects</p>
@@ -15,5 +15,6 @@ export default class Bio extends React.Component {
 }
 
 Bio.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  id: PropTypes.string
 }
