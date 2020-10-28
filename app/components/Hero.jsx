715a35f8791159d117187eeef69d0a7192a0aeba
Henri Huuskonen <henri.huuskonen@hotmail.com>
2020-10-28 17:40:52 +0200

    Add alt and avatar css tweaks

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -16,7 +16,7 @@ export default class Hero extends React.Component {
             src="https://t3.ftcdn.net/jpg/02/80/09/80/240_F_280098052_VGr2NfQI6jAXqy7KzBoswbY95yGtp1cB.jpg"/>
         </div>
         <div className="hero__avatar-container">
-          <img className="hero__avatar--image" src="https://www.dropbox.com/s/ysyoqnfqtfhqvjh/henrihuuskonen.png?raw=1"/>
+          <img className="hero__avatar--image" alt="Avatar image of Henri Huuskonen" src="https://www.dropbox.com/s/ysyoqnfqtfhqvjh/henrihuuskonen.png?raw=1"/>
         </div>
       </div>
     )
